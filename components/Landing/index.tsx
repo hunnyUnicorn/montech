5fc3d9ceb9dd1be4b090a8c96265a7e641ba34eb
Hunny Unicorn <hunnyunicorn1@gmail.com>
2022-11-27 08:22:16 -0500

    fix config
    Co-authored-by: hunnystar <108871459+hunnystar@users.noreply.github.com>
    Co-authored-by: Hunnyboy1217 <110440428+Hunnyboy1217@users.noreply.github.com>
    Co-authored-by: MuscleBunny <103564801+MuscleBunny@users.noreply.github.com>

--- components/Landing/index.tsx
+++ components/Landing/index.tsx
@@ -3,7 +3,12 @@ import MainLayout from "@/layout/Main"
 const Landing = () => {
   return (
     <MainLayout>
-      Welcome
+      <div>
+        Welcome to Chess Games
+      </div>
+      <div>
+        Log in with your wallet to be able to authenticate and play games
+      </div>
     </MainLayout>
   )
 }
--- layout/Main.tsx
+++ layout/Main.tsx
@@ -1,16 +1,20 @@
+import { twMerge } from 'tailwind-merge'
 import Header from './Header'
 
 interface Props {
+  className?: string
   children?: React.ReactNode
 }
 
-const MainLayout = ({children}: Props) => {
+const MainLayout = ({className, children}: Props) => {
   return (
     <div className='px-16'>
       <Header />
       <div className='fixed top-0 left-0 w-screen h-screen bg-[url("../public/board.jpg")] z-[-10]'/>
       <main className='w-full h-full flex items-center justify-center'>
-        {children}
+        <div>
+          {children}
+        </div>
       </main>
     </div>
   )
